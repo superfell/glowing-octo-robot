b62678e7a854703843de617c2b0b0c00149b42ed
Simon Fell <fellforce@gmail.com>
2021-02-12 21:01:31 -0800

    change value to Node<T> and fix all clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::usize;
 
 trait Container<T> {
     fn get_value(&self) -> Option<&T>;
-    fn get_value_slot(&mut self) -> &mut Option<Node<T>>;
+    fn get_value_slot(&mut self) -> &mut Node<T>;
     fn set_value(&mut self, v: Node<T>);
 
     fn get_keys(&self) -> Vec<u8>;
@@ -29,14 +29,14 @@ struct Container4<T> {
     keys: [u8; 4],
     children: [Node<T>; 4],
     count: usize,
-    value: Option<Node<T>>,
+    value: Node<T>,
 }
 
 impl<T> Container4<T> {
     fn new() -> Self {
         Container4::<T> {
             children: [Node::None, Node::None, Node::None, Node::None],
-            value: None,
+            value: Node::None,
             count: 0,
             keys: [0; 4],
         }
@@ -52,11 +52,7 @@ impl<T> Container<T> for Container4<T> {
             return false;
         }
         let c = self.get_child(key);
-        if let Node::None = c {
-            true
-        } else {
-            false
-        }
+        matches!(c, Node::None)
     }
     fn get_child(&self, key: u8) -> &Node<T> {
         for i in 0..self.count {
@@ -78,36 +74,34 @@ impl<T> Container<T> for Container4<T> {
         let idx = self.count;
         self.keys[idx] = key;
         self.count += 1;
-        return &mut self.children[idx];
+        &mut self.children[idx]
     }
 
     fn get_value(&self) -> Option<&T> {
         match &self.value {
-            Some(n) => match n {
-                Node::Leaf(v) => Some(v),
-                _ => panic!("A Nodes value should always be a Leaf"),
-            },
-            None => None,
+            Node::Leaf(v) => Some(v),
+            Node::None => None,
+            _ => panic!("A Nodes value should always be a Leaf"),
         }
     }
-    fn get_value_slot(&mut self) -> &mut Option<Node<T>> {
+    fn get_value_slot(&mut self) -> &mut Node<T> {
         &mut self.value
     }
     fn set_value(&mut self, v: Node<T>) {
-        self.value = Some(v);
+        self.value = v;
     }
 }
 
 struct Container256<T> {
     children: [Node<T>; 256],
-    value: Option<Node<T>>,
+    value: Node<T>,
 }
 
 impl<T> Container256<T> {
     fn new(src: &mut Box<dyn Container<T>>) -> Container256<T> {
         let mut n = Container256 {
             children: arr![Node::None; 256],
-            value: None,
+            value: Node::None,
         };
         src.get_keys().into_iter().for_each(|k| {
             let x = src.get_child_slot(k);
@@ -115,18 +109,16 @@ impl<T> Container256<T> {
             std::mem::swap(x, y);
         });
         std::mem::swap(src.get_value_slot(), n.get_value_slot());
-        return n;
+        n
     }
 }
 
 impl<T> Container<T> for Container256<T> {
     fn get_value(&self) -> Option<&T> {
         match &self.value {
-            Some(n) => match n {
-                Node::Leaf(v) => Some(v),
-                _ => panic!("A Nodes value should always be a Leaf"),
-            },
-            None => None,
+            Node::Leaf(v) => Some(v),
+            Node::None => None,
+            _ => panic!("A Nodes value should always be a Leaf"),
         }
     }
     fn should_grow(&self, _key: u8) -> bool {
@@ -136,7 +128,7 @@ impl<T> Container<T> for Container256<T> {
         self.children
             .iter()
             .enumerate()
-            .filter(|(_, n)| if let Node::None = n { false } else { true })
+            .filter(|(_, n)| !matches!(n, Node::None))
             .map(|(i, _)| i as u8)
             .collect()
     }
@@ -146,9 +138,9 @@ impl<T> Container<T> for Container256<T> {
     }
 
     fn set_value(&mut self, v: Node<T>) {
-        self.value = Some(v);
+        self.value = v;
     }
-    fn get_value_slot(&mut self) -> &mut Option<Node<T>> {
+    fn get_value_slot(&mut self) -> &mut Node<T> {
         &mut self.value
     }
     fn get_child_slot(&mut self, key: u8) -> &mut Node<T> {
@@ -160,6 +152,12 @@ pub struct Tree<T> {
     root: Node<T>,
 }
 
+impl<T: 'static> Default for Tree<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<T: 'static> Tree<T> {
     pub fn new() -> Self {
         Self { root: Node::None }
@@ -168,7 +166,7 @@ impl<T: 'static> Tree<T> {
     pub fn put(&mut self, key: &[u8], value: T) {
         let mut n = &mut self.root;
         let mut k = key;
-        while k.len() > 0 {
+        while !k.is_empty() {
             if let Node::Container(c) = n {
                 if c.should_grow(k[0]) {
                     *n = Node::Container(Box::new(Container256::new(c)));
@@ -208,14 +206,14 @@ impl<T: 'static> Tree<T> {
         loop {
             match n {
                 Node::Leaf(t) => {
-                    if k.len() == 0 {
+                    if k.is_empty() {
                         return Some(t);
                     } else {
                         return None;
                     }
                 }
                 Node::Container(c) => {
-                    if k.len() == 0 {
+                    if k.is_empty() {
                         return c.get_value();
                     }
                     n = c.get_child(k[0]);
