3de03080f8dbdcab4ef7fe71e4050ff42eaf57a4
Simon Fell <fellforce@gmail.com>
2021-02-13 00:17:10 -0800

    Implement path compression as another Node type

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use arr_macro::arr;
-use std::usize;
+use std::cmp::min;
 
 enum Node<T> {
     None,
     Leaf(T),
+    Path(Box<Path<T>>),
     Container4(Box<Container4<T>>),
     Container256(Box<Container256<T>>),
 }
@@ -24,6 +25,61 @@ impl<T> Default for Node<T> {
     }
 }
 
+struct Path<T> {
+    key: Vec<u8>,
+    child: Node<T>,
+}
+
+// wrap_path_in_container returns a new container that contains the path.
+fn wrap_path_in_container<T>(n: &mut Node<T>) -> Box<Container4<T>> {
+    if let Node::Path(p) = n {
+        let mut c = Box::new(Container4::<T> {
+            keys: [p.key.remove(0), 0, 0, 0],
+            children: [Node::None, Node::None, Node::None, Node::None],
+            count: 1,
+            value: Node::None,
+        });
+        // path.key might be empty now, in which case we can put the path child directly
+        // into the new container rather than the remaining path
+        c.children[0] = if p.key.is_empty() {
+            std::mem::take(&mut p.child)
+        } else {
+            std::mem::take(n)
+        };
+        c
+    } else {
+        panic!("wrap_path_in_container should be called with a Node::Path");
+    }
+}
+
+fn split_path_maybe<T>(n: &mut Node<T>, k: &[u8]) -> Option<Node<T>> {
+    if let Node::Path(p) = n {
+        // if k has a prefix of p, then we're okay, otherwise we need
+        // to split this path where the common prefix ends.
+
+        let prefixlen = common_prefix_len(k, p.key.as_slice());
+        if prefixlen < p.key.len() {
+            // mutate path(child) to path(container(path(child)))
+            let head = p.key[..prefixlen].to_vec();
+            p.key = p.key[prefixlen..].to_vec();
+            let c = wrap_path_in_container(n);
+            // its possible that there's no matching prefix at all.
+            // In that case we can use the container we just built as the result
+            // otherwise we need a path to the container
+            return if head.is_empty() {
+                Some(Node::Container4(c))
+            } else {
+                Some(Node::Path(Box::new(Path {
+                    key: head,
+                    child: Node::Container4(c),
+                })))
+            };
+        }
+        None
+    } else {
+        panic!("split_path_maybe should be called with a Node::Path");
+    }
+}
 struct Container4<T> {
     keys: [u8; 4],
     children: [Node<T>; 4],
@@ -113,9 +169,26 @@ impl<T: 'static> Tree<T> {
                     *n = Node::Container256(Box::new(Container256::new(c)));
                 }
             }
+            if let Node::Path(_) = n {
+                if let Some(new_node) = split_path_maybe(n, k) {
+                    *n = new_node
+                }
+            }
             match n {
                 Node::None => {
-                    *n = Node::Container4(Box::new(Container4::new()));
+                    // We've reached the bottom of the tree for this branch, we can create
+                    // a path + leaf to handle the remaining key
+                    *n = Node::Path(Box::new(Path {
+                        key: k.to_vec(),
+                        child: Node::Leaf(value),
+                    }));
+                    return;
+                }
+                Node::Path(p) => {
+                    // at this point p must be a prefix of k. (due to split_path_maybe call above)
+                    assert!(k.starts_with(p.key.as_slice()));
+                    n = &mut p.child;
+                    k = &k[p.key.len()..];
                 }
                 Node::Leaf(_) => {
                     let mut c = Box::new(Container4::new());
@@ -139,6 +212,11 @@ impl<T: 'static> Tree<T> {
             Node::Leaf(v) => {
                 *v = value;
             }
+            Node::Path(_) => {
+                let mut c = wrap_path_in_container(n);
+                c.value = Node::Leaf(value);
+                *n = Node::Container4(c);
+            }
             Node::Container4(c) => {
                 c.value = Node::Leaf(value);
             }
@@ -160,6 +238,16 @@ impl<T: 'static> Tree<T> {
                         return None;
                     }
                 }
+                Node::Path(p) => {
+                    if k.len() < p.key.len() {
+                        return None;
+                    }
+                    if p.key.as_slice() != &k[..p.key.len()] {
+                        return None;
+                    }
+                    n = &p.child;
+                    k = &k[p.key.len()..]
+                }
                 Node::Container4(c) => {
                     if k.is_empty() {
                         return c.value.leaf_value();
@@ -180,6 +268,16 @@ impl<T: 'static> Tree<T> {
     }
 }
 
+fn common_prefix_len(a: &[u8], b: &[u8]) -> usize {
+    let len = min(a.len(), b.len());
+    for i in 0..len {
+        if a[i] != b[i] {
+            return i;
+        }
+    }
+    len
+}
+
 #[cfg(test)]
 mod test {
     use super::Tree;
@@ -189,16 +287,19 @@ mod test {
         let mut x = Tree::<String>::new();
         x.put(&[1, 2], String::from("alice"));
         x.put(&[1, 2, 3], String::from("bob"));
-        x.put(&[2, 10, 11], String::from("eve"));
+        x.put(&[2, 10, 11, 12, 13, 14], String::from("eve"));
+        x.put(&[2, 10, 11], String::from("jim"));
 
         assert_eq!(x.get(&[1, 2]), Some(&String::from("alice")));
         assert_eq!(x.get(&[1, 2, 3]), Some(&String::from("bob")));
-        assert_eq!(x.get(&[2, 10, 11]), Some(&String::from("eve")));
+        assert_eq!(x.get(&[2, 10, 11, 12, 13, 14]), Some(&String::from("eve")));
+        assert_eq!(x.get(&[2, 10, 11]), Some(&String::from("jim")));
 
         assert_eq!(x.get(&[5]), None);
         assert_eq!(x.get(&[]), None);
         assert_eq!(x.get(&[2, 10]), None);
         assert_eq!(x.get(&[2, 10, 11, 12]), None);
+        assert_eq!(x.get(&[2, 10, 11, 12, 13, 14, 15]), None);
     }
 
     #[test]
